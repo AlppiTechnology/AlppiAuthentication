fd531b7bd71e92592efab51d7ec31534d2602eac
Patrick Piccini <patrickbpiccini@hotmail.com>
2024-02-29 18:56:57 -0300

    documentation: documentação de rotas para login

--- apps/authentication/login/validations.py
+++ apps/authentication/login/validations.py
@@ -4,6 +4,18 @@ UserModel = get_user_model()
 
 
 def validate_registration(data) -> bool:
+    """
+    Validação para garantir que o número de matrícula seja fornecido.
+
+    Esta função verifica se o número de matrícula foi fornecido nos dados recebidos.
+    Se não, levanta uma exceção de validação.
+
+    Parâmetros:
+    - data (dict): Dados contendo a chave 'registration' que representa o número de matrícula.
+
+    Retorna:
+    bool: True se o número de matrícula for fornecido corretamente.
+    """
     registration = data['registration'].strip()
     if not registration:
         raise ValidationError('Informe o numero de matricula para o login.')
@@ -11,6 +23,18 @@ def validate_registration(data) -> bool:
 
 
 def validate_password(data) -> bool:
+    """
+    Validação para garantir que a senha seja fornecida.
+
+    Esta função verifica se a senha foi fornecida nos dados recebidos.
+    Se não, levanta uma exceção de validação.
+
+    Parâmetros:
+    - data (dict): Dados contendo a chave 'password' que representa a senha.
+
+    Retorna:
+    bool: True se a senha for fornecida corretamente.
+    """
     password = data['password'].strip()
     if not password:
         raise ValidationError('Informe a senha para o login.')
@@ -18,11 +42,20 @@ def validate_password(data) -> bool:
 
 
 def validate_level_group(user_group) -> str:
+    """
+    Validação para determinar o nível de acesso do grupo de usuário.
+
+    Esta função verifica o grupo de usuário fornecido e determina o nível de acesso correspondente.
+    Retorna o primeiro nível de acesso encontrado no grupo ou None se nenhum for encontrado.
+
+    Parâmetros:
+    - user_group (str): Grupo de usuário que pode conter informações sobre o nível de acesso.
+
+    Retorna:
+    str or None: O nível de acesso encontrado ou None se nenhum for encontrado.
+    """
     group_list = ['superuser','administrador','coordenador',
                   'avaliador','professor','aluno']
 
-    for group in group_list:
-        if group in user_group:
-            return group
-        
+
     return next((group for group in group_list if group in user_group), None)
