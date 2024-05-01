15b58d8bb2f862af4cf9f0dbd90081943505f92a
Patrick Piccini <patrickbpiccini@hotmail.com>
2024-05-01 15:50:55 -0300

    fix: mudança de rota para authentication

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('ms1/', include('api.v1.urls'))
+    path('auth/', include('api.v1.urls'))
 ]
